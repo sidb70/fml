eec9e4a75d34c1f482c05dd8652fab572b558a11
Uzair <uzair.m6d@gmail.com>
2025-02-08 20:22:34 -0500

    refactor rank posts

--- rank_posts.py
+++ rank_posts.py
@@ -1,27 +1,46 @@
 import numpy as np
 import scipy as sp
+from typing import List
+from bertopic import BERTopic
 
 
-def rank(posts, preferences, n_posts, temperature=1.0):
+def rank_posts(all_posts: List[str], preferences: np.ndarray, topic_model: BERTopic, top_n_posts, temperature=1.0):
     """
-    Ranks posts according to a user's preferences.
+    Ranks the top n posts according to a user's preferences.
     
     Args:
-        posts: array of shape (n, d), where d is the dimension of topics
+        posts: list of strings, the text of the posts
         preferences: array of shape (d,) of user preferences
+        topic_model: BERTopic, the topic model to use for determining the post's topic representation
         n_posts: number of posts to return
     """
 
+
+    post_representations = np.array([generate_post_representation(post, topic_model) for post in all_posts]) # array of shape (n_posts, n_topics)
+
     # Dot product between posts and preferences, then softmax
-
-    out = np.dot(posts, preferences)
-
+    out = np.dot(post_representations, preferences)
     normalized = sp.special.softmax(out / temperature)
 
     # randomly sample n_posts from the normalized distribution, without replacement
-    indices = np.random.choice(len(posts), size=n_posts, replace=False, p=normalized)
+    indices = np.random.choice(len(all_posts), size=top_n_posts, replace=False, p=normalized)
 
     return indices
+
+
+def generate_post_representation(post, topic_model: BERTopic):
+    """
+    Returns a vector of length n_topics, where each element represents the post's relevance to that topic.
+
+    Args:
+        post: string, the text of the post
+        topic_model: BERTopic, the topic model to use for determining the post's topic representation
+
+    Returns:
+        post_representation: array of shape (n_topics,) representing the post's relevance
+    """
+
+    return topic_model.transform([post])
 
 
 
@@ -41,5 +60,4 @@ if __name__ == "__main__":
 
     temperature = 1.0
 
-    print(rank(post_topics, preferences, n_posts, temperature))
-
+    print(rank_posts(post_topics, preferences, n_posts, temperature))
