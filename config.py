80940a932c5f20cda9a8ef7a645936b66e65745d
Uzair <uzair.m6d@gmail.com>
2025-02-08 20:14:49 -0500

    add config.py

---
+++ config.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+import yaml
+import os
+
+@dataclass
+class Config:
+    device: str = "cuda"
+    model_path: str = "data/topic_model"
+    docs_path: str = "data/docs"
+    posts_path: str = "data/posts.jsonl"
+    sample_size: int = 5000
+
+# try to load "config.yaml" file
+
+config_path = "config.yaml"
+
+config = None
+
+try: 
+    with open(config_path, "r") as f:
+        config_dict = yaml.safe_load(f)
+        config = Config(**config_dict)
+except FileNotFoundError:
+    # create a new config object and save it to "config.yaml"
+    config = Config()
+    with open(config_path, "w") as f:
+        yaml.safe_dump(config.__dict__, f)
+
+
+
